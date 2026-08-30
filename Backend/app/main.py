f6eafd1b00bc4f0dcaf7ce8566ca4c93c14be25a
santorar <estractor179@gmail.com>
2026-08-30 15:28:17 -0500

    feat: añadidas nuevas rutas para empresa, usuario, estado y tercero feat: añadida dependencia de bcrypt

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from app.routes import puc, comprobante, periodo_contable
+from app.routes import puc, comprobante, periodo_contable, empresa, usuario, estado, tercero
 
 app = FastAPI(title="Api Prueba Técnica Esguerra JHR - BalanceApp")
 
@@ -8,3 +8,8 @@ app = FastAPI(title="Api Prueba Técnica Esguerra JHR - BalanceApp")
 app.include_router(puc.router)
 app.include_router(comprobante.router)
 app.include_router(periodo_contable.router)
+app.include_router(empresa.router)
+app.include_router(usuario.router)
+app.include_router(estado.router)
+app.include_router(tercero.router)
+app.include_router(tercero.router_tipos_documento)
